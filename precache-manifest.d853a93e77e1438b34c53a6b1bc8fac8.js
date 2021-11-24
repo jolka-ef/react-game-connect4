7ebaea59130088c984427a8c03694cb736cd26b4
Agata <a.firlejczyk@gmail.com>
2021-11-24 16:43:43 +0100

    Updates

---
+++ precache-manifest.d853a93e77e1438b34c53a6b1bc8fac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639ed4003b279a0dd43cbb5a1ee7ec60",
+    "url": "/react-game-connect4/index.html"
+  },
+  {
+    "revision": "acff268dd529ab6de8d5",
+    "url": "/react-game-connect4/static/css/main.cb953ed7.chunk.css"
+  },
+  {
+    "revision": "3faa372b7c26e6619f7a",
+    "url": "/react-game-connect4/static/js/2.699dcc91.chunk.js"
+  },
+  {
+    "revision": "acff268dd529ab6de8d5",
+    "url": "/react-game-connect4/static/js/main.2ce2f18b.chunk.js"
+  },
+  {
+    "revision": "6b2476de2578a5a980b0",
+    "url": "/react-game-connect4/static/js/runtime~main.8c5820b1.js"
+  }
+]);
