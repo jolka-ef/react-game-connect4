dab5b8262aa2c7c7367ab07d3ed86a5b28e95fdc
Jolka <firjola@gmail.com>
2023-09-26 19:47:23 +0200

    gh pages

---
+++ precache-manifest.969d70b786c1b26049b7074443e398e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d449d7f433a86fe48f5ac24a707cfe",
+    "url": "/react-game-connect4/index.html"
+  },
+  {
+    "revision": "a5a6ef45955ece52884d",
+    "url": "/react-game-connect4/static/css/main.cb953ed7.chunk.css"
+  },
+  {
+    "revision": "3faa372b7c26e6619f7a",
+    "url": "/react-game-connect4/static/js/2.699dcc91.chunk.js"
+  },
+  {
+    "revision": "a5a6ef45955ece52884d",
+    "url": "/react-game-connect4/static/js/main.b01108c9.chunk.js"
+  },
+  {
+    "revision": "6b2476de2578a5a980b0",
+    "url": "/react-game-connect4/static/js/runtime~main.8c5820b1.js"
+  }
+]);
